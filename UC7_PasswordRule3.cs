53b538f54acee178a855374d3231a7591d1cc388
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 10:11:36 +0530

    Password with at least one digit Regex Validation

---
+++ UC7_PasswordRule3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class UC7_PasswordRule3
+    {
+        // A password should have at least one numeric character
+        public static string Regex_Password = "(?=.*[0-9])(?=.[A-Za-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
