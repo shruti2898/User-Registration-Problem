faf919a3f403fc271732b5703aa7fec06d7fae6b
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 10:08:59 +0530

    Mobile Number Regex Validation

---
+++ UC4_MobileNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class UC4_MobileNumber
+	{  
+		//Mobile Format - 91 9876543210
+	   //Country code follow by space and 10 digit number
+
+		public static string Regex_MobileNumber = "^[1-9][0-9][ ]?[6-9][0-9]{9}$";
+
+		public bool validateMobileNumber(string mobileNumber)
+		{
+			return Regex.IsMatch(mobileNumber, Regex_MobileNumber);
+		}
+	}
+}
