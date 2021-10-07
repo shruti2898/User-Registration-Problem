d56aa4654f83442f0dc916faf112974d06d7285d
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 10:12:42 +0530

    Password with exactly one special character Regex Validation

---
+++ UC8_PasswordRule4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class UC8_PasswordRule4
+    {  
+        // A password should have exactly one special character
+        public static string Regex_Password = "(?=.*[!@#$%^&*]{,1})(?=.[A-Za-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
