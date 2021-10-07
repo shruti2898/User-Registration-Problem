58b3d7f1cfce4156f22f6f665bb6747904b2a17b
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 09:55:39 +0530

    First Name Regex Validation

---
+++ UC1_FirstName.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+	class UC1_FirstName
+	{   
+		// first name should start with a capital alphabet and has minimum 3 characters(including capital alphabet)
+		public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+
+		public bool validateFirstName(string firstName)
+		{
+			return Regex.IsMatch(firstName, Regex_FirstName); 
+		}
+	}
+}
