2658f0e59301e7bcf06a08d119295db447bc9bbe
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 10:09:45 +0530

    Password with minimum 8 characters Regex Validation

---
+++ UC5_PasswordRule1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class UC5_PasswordRule1
+    {  
+        // A password should have minimum 8 characters 
+        public static string Regex_Password = "^[a-zA-z0-9]{8,16}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
