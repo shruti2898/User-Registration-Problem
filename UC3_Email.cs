943adeb01c9518f506b0a9687fa3e3911346b390
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 09:59:04 +0530

    Email Regex Validation

---
+++ UC3_Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+	class UC3_Email
+	{
+		// Email has 3 mandatory parts (abc, bl & co) and 2 optional(xyz & in) with precise @ and. positions
+		//E.g. abc.xyz@bl.co.in
+		public static string Regex_Email = "^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}([.][A-Za-z]{2})?$";
+
+		public bool validateEmail(string email)
+		{
+			return Regex.IsMatch(email, Regex_Email);
+		}
+	}
+}
