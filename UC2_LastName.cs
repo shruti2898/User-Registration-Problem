3dd7038eb4156419cca00136a600afa75c8a98f0
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 09:57:07 +0530

    Last Name Regex Validation

---
+++ UC2_LastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+	class UC2_LastName
+	{
+		// last name should start with a capital alphabet and has minimum 3 characters(including capital alphabet)
+
+		public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
+
+		public bool validateLastName(string lastName)
+		{
+			return Regex.IsMatch(lastName, Regex_LastName);
+		}
+	}
+}
