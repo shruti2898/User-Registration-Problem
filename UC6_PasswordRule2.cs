730605983ec5f92bd8ac91c29516b96ab9bdd56e
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 10:10:40 +0530

    Password with at least one uppercase Regex Validation

---
+++ UC6_PasswordRule2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class UC6_PasswordRule2
+    {
+        // A password should have at least one uppercase character
+        public static string Regex_Password = "(?=.*[A-Z])(?=.[0-9a-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
